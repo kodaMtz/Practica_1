27f64e14859ea585071dde6b5d282ded6684cc97
unknown <k2v3nkevoneto@gmail.com>
2025-02-05 07:36:30 -0600

    Hola Mundo

---
+++ src/js/Script2.js
@@ -0,0 +1,112 @@
+//SINTAXIS E IDENTIFICADORES
+
+/*console.log("Hola soy un script independiente con gustos bien dementes")
+alert("Hola soy una alerta desde script independiente")*/
+
+/*let nombre = prompt("Ingresa tu nombre")
+console.log(nombre);*/
+
+/*Otra forma de declarar una sentencia
+let variable = 45;*/
+
+// <--- comentario de una sola linea
+
+/*
+  <--- Comentario de bloque 
+*/
+
+/**
+ * <--- Este es un comentario de documentacion
+ */
+
+//ASIGNACION DE VARIABLE
+let number = 56;
+
+//EXPRESION ARITMETICA
+let multiplicacion = 5 * 32
+let x;
+let expresion = 5(x)+3(x)+1;
+
+//Llamada de una funcion
+Sumar();
+Calcular_Prom();
+
+//Estructura de una funcion 
+function Sumar(x,y){
+
+}
+
+//Estructura de una funcion con valor de return
+function getPromedio(){
+
+    return promedio,x,y;
+}
+
+function getMessage(){
+    return "Hola"
+}
+
+//ESTRUCTURAS DE CONTROL
+
+//If de estructura selectiva simple
+if(Condicion){
+
+}
+
+//If Estructura selectiva doble
+if(Condicion){
+    
+
+} else {
+
+}
+
+//If estructura selectiva multiple
+if(Condicion){
+
+
+}else if(Condicion){
+
+}
+// if estructura selectiva multple simple
+if(Condicion){
+    if(Condicion){
+
+    }else{
+
+    }
+}else{
+
+}
+
+if(Condicion){
+
+}else if (Condicion){
+
+}else if(Condicion){
+
+}
+
+//BUCLES
+for(let i = 0; i <= 10; i++){
+
+}
+
+//Estructura de una clase
+class MiClase{
+    Nombre = "Kevin";
+    Edad = "26";
+
+    constructor(apellidoP, apellidoM, gender){
+        this.apellidoP = apellidoP;
+        this.apellidoM = apellidoM;
+        this.gender = "F";
+    }
+
+}
+
+//Manipulacion de objetos y propiedades
+persona.numControl = "02025465";
+
+
+//CREAR UN OBJETO
